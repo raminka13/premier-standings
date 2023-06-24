0bc25e26e9975974aa63ae2d55829e9c67ad2f95
Raul Ospina <raminka13@hotmail.com>
2023-06-24 12:01:01 -0500

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        Made with ❤️ by
+        <a href="https://raminka13.github.io/littlelink/" target="_blank" rel="noreferrer"> raminka</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
